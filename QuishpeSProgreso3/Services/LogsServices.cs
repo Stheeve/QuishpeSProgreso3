4aa512e5897b3f7a33124e74e88f7a384c064b7e
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-07-10 08:02:50 -0500

    Se agrego una carpeta Services donde se agregaron dos clases una de logs y otra de producto

---
+++ QuishpeSProgreso3/Services/LogsServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuishpeSProgreso3.Services
+{
+    public class LogsServices
+    {
+        private readonly string _filePath;
+
+        public LogsServices()
+        {
+            var filename = $"Logs_Quishpe.txt";
+            _filePath = Path.Combine(FileSystem.AppDataDirectory, filename);
+        }
+
+        public async Task AppendLogAsync(string producto)
+        {
+            string log = $"Se incluyó el registro [{producto}] el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            await File.AppendAllTextAsync(_filePath, log);
+        }
+
+        public async Task<string> LeerLogsAsync()
+        {
+            if (File.Exists(_filePath))
+                return await File.ReadAllTextAsync(_filePath);
+            return "No hay logs.";
+        }
+    }
+}
---
+++ QuishpeSProgreso3/Services/ProductoServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuishpeSProgreso3.Models;
+using SQLite;
+
+namespace QuishpeSProgreso3.Services
+{
+    public class ProductoServices
+    {
+        private SQLiteAsyncConnection _db;
+
+        public async Task Init()
+        {
+            if (_db != null)
+                return;
+
+            var path = Path.Combine(FileSystem.AppDataDirectory, "productos.db");
+            _db = new SQLiteAsyncConnection(path);
+            await _db.CreateTableAsync<Producto>();
+        }
+
+        public async Task<List<Producto>> GetProductosAsync()
+        {
+            await Init();
+            return await _db.Table<Producto>().ToListAsync();
+        }
+
+        public async Task AddProductoAsync(Producto producto)
+        {
+            await Init();
+            await _db.InsertAsync(producto);
+        }
+    }
+}
