85b54d2499c50f15605bab0125a6359515aafb88
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-07-10 08:15:30 -0500

    Se agrego dentro de la carpeta view models una clase logs

---
+++ QuishpeSProgreso3/ViewModels/LogsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuishpeSProgreso3.Services;
+
+namespace QuishpeSProgreso3.ViewModels
+{
+    public class LogsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _contenidoLog;
+        public string ContenidoLog
+        {
+            get => _contenidoLog;
+            set
+            {
+                _contenidoLog = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ContenidoLog)));
+            }
+        }
+
+        private readonly LogsServices _logsServices = new();
+
+        public LogsViewModel()
+        {
+            CargarLogs();
+        }
+
+        private async void CargarLogs()
+        {
+            ContenidoLog = await _logsServices.LeerLogsAsync();
+        }
+    }
+}
