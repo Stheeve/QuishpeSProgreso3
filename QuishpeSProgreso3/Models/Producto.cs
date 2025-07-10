600fd188349a92117ad906b58b96d0107a403c57
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-07-10 07:46:19 -0500

    Se creo una carpeta Models y se agrego la clase producto donde se puso los atributos que se van a usar

---
+++ QuishpeSProgreso3/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace QuishpeSProgreso3.Models
+{
+    public class Producto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Categoria { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal PrecioEstimado { get; set; }
+    }
+}
